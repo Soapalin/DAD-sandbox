36ed60eba0f550fbce604742f3763f2d6d252381
liamw99 <liamwarburton99@hotmail.com>
2019-05-25 12:30:06 +1000

    ADC Init completed, needs testing

--- TempController_template/Debug/drivers/cfal96x64x16.d
+++ TempController_template/Debug/drivers/cfal96x64x16.d
@@ -1,15 +1,15 @@
 # FIXED
 
 drivers/cfal96x64x16.obj: ../drivers/cfal96x64x16.c
-drivers/cfal96x64x16.obj: D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdbool.h
-drivers/cfal96x64x16.obj: D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdint.h
-drivers/cfal96x64x16.obj: D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/_stdint40.h
-drivers/cfal96x64x16.obj: D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/stdint.h
-drivers/cfal96x64x16.obj: D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/cdefs.h
-drivers/cfal96x64x16.obj: D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_types.h
-drivers/cfal96x64x16.obj: D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_types.h
-drivers/cfal96x64x16.obj: D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_stdint.h
-drivers/cfal96x64x16.obj: D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_stdint.h
+drivers/cfal96x64x16.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdbool.h
+drivers/cfal96x64x16.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdint.h
+drivers/cfal96x64x16.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/_stdint40.h
+drivers/cfal96x64x16.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/stdint.h
+drivers/cfal96x64x16.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/cdefs.h
+drivers/cfal96x64x16.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_types.h
+drivers/cfal96x64x16.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_types.h
+drivers/cfal96x64x16.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_stdint.h
+drivers/cfal96x64x16.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_stdint.h
 drivers/cfal96x64x16.obj: C:/Software/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
 drivers/cfal96x64x16.obj: C:/Software/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h
 drivers/cfal96x64x16.obj: C:/Software/ti/TivaWare_C_Series-2.1.4.178/driverlib/ssi.h
@@ -20,15 +20,15 @@ drivers/cfal96x64x16.obj: C:/Software/ti/TivaWare_C_Series-2.1.4.178/grlib/grlib
 drivers/cfal96x64x16.obj: C:/Software/ti/TivaWare_C_Series-2.1.4.178/examples/boards/ek-lm4f232/drivers/cfal96x64x16.h
 
 ../drivers/cfal96x64x16.c: 
-D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdbool.h: 
-D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdint.h: 
-D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/_stdint40.h: 
-D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/stdint.h: 
-D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/cdefs.h: 
-D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_types.h: 
-D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_types.h: 
-D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_stdint.h: 
-D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_stdint.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdbool.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdint.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/_stdint40.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/stdint.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/cdefs.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_types.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_types.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_stdint.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_stdint.h: 
 C:/Software/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h: 
 C:/Software/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h: 
 C:/Software/ti/TivaWare_C_Series-2.1.4.178/driverlib/ssi.h: 
--- TempController_template/Debug/utils/uartstdio.d
+++ TempController_template/Debug/utils/uartstdio.d
@@ -1,16 +1,16 @@
 # FIXED
 
 utils/uartstdio.obj: ../utils/uartstdio.c
-utils/uartstdio.obj: D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdbool.h
-utils/uartstdio.obj: D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdint.h
-utils/uartstdio.obj: D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/_stdint40.h
-utils/uartstdio.obj: D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/stdint.h
-utils/uartstdio.obj: D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/cdefs.h
-utils/uartstdio.obj: D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_types.h
-utils/uartstdio.obj: D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_types.h
-utils/uartstdio.obj: D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_stdint.h
-utils/uartstdio.obj: D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_stdint.h
-utils/uartstdio.obj: D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdarg.h
+utils/uartstdio.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdbool.h
+utils/uartstdio.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdint.h
+utils/uartstdio.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/_stdint40.h
+utils/uartstdio.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/stdint.h
+utils/uartstdio.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/cdefs.h
+utils/uartstdio.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_types.h
+utils/uartstdio.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_types.h
+utils/uartstdio.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_stdint.h
+utils/uartstdio.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_stdint.h
+utils/uartstdio.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdarg.h
 utils/uartstdio.obj: C:/Software/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h
 utils/uartstdio.obj: C:/Software/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
 utils/uartstdio.obj: C:/Software/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h
@@ -24,16 +24,16 @@ utils/uartstdio.obj: C:/Software/ti/TivaWare_C_Series-2.1.4.178/driverlib/uart.h
 utils/uartstdio.obj: C:/Software/ti/TivaWare_C_Series-2.1.4.178/utils/uartstdio.h
 
 ../utils/uartstdio.c: 
-D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdbool.h: 
-D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdint.h: 
-D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/_stdint40.h: 
-D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/stdint.h: 
-D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/cdefs.h: 
-D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_types.h: 
-D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_types.h: 
-D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_stdint.h: 
-D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_stdint.h: 
-D:/Program//\ Files//\ (x86)/ti/css901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdarg.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdbool.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdint.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/_stdint40.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/stdint.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/cdefs.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_types.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_types.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_stdint.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_stdint.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdarg.h: 
 C:/Software/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h: 
 C:/Software/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h: 
 C:/Software/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h: 
